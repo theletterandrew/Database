c957469b87d52ebec848e74a4737880814e95f98
Andrew Phillips <andrew.austen.phillips@gmail.com>
2017-10-31 09:36:12 -0500

    Changed the search function to use a HashMap instead of iterating through the whole list.

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,16 @@ public class Main {
         jameson.setName("Jameson");
         jameson.setPercentLeft(100.00);
         jameson.setYear(2000);
-        myManager.addBottle(jameson);
+        myManager.addBottle(jameson, "1");
         Wine riesling = new Wine();
         riesling.setWineType("Riesling");
         riesling.setCountry("Germany");
         riesling.setName(" Egon Muller Scharzhofberger Spatlese Riesling");
         riesling.setPercentLeft(100.00);
         riesling.setYear(2015);
-        myManager.addBottle(riesling);
+        myManager.addBottle(riesling, "2");
 
-        Bottle result = myManager.searchBottles("Jameson");
+        Bottle result = myManager.searchBottles("1");
         System.out.println(result.name);
     }
 }
--- src/Manager.java
+++ src/Manager.java
@@ -1,28 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Manager {
-    Bottle [] myBottles;
+    HashMap<String, Bottle> allBottles;
+    ArrayList<Bottle> myBottles;
     int bottleCount;
     Manager(){
         this.bottleCount = 0;
-        this.myBottles = new Bottle[500];
+        allBottles = new HashMap<String, Bottle>();
+        myBottles = new ArrayList<Bottle>();
     }
-    void addBottle(Bottle bottle){
-        myBottles[bottleCount] = bottle;
+    void addBottle(Bottle bottle, String id){
+        myBottles.add(bottle);
+        allBottles.put(id, bottle);
         bottleCount++;
     }
-    Bottle searchBottles(String searchName){
-        for(int i = 0; i<bottleCount; i++){
-            if(myBottles[i].name.equals(searchName)){
-                return myBottles[i];
-            }
-        }
-        return null;
+    Bottle searchBottles(String id){
+        Bottle bottle = allBottles.get(id);
+        return bottle;
     }
-    Bottle searchYear(int year){
-        for(int i=0; i<bottleCount; i++){
-            if(myBottles[i].year == year){
-                return myBottles[i];
-            }
-        }
-        return null;
-    }
+
+
 }
